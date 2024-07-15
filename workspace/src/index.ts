7c47385950ca00d91137cfef2b90cdec2735c2df
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-16 03:36:27 +0900

    CodeをちゃんとCommit

---
+++ workspace/src/index.ts
@@ -0,0 +1,40 @@
+import {Hono} from 'hono'
+import { type ExportedHandlerScheduledHandler } from "@cloudflare/workers-types"
+
+const app = new Hono<{
+  Bindings: {
+    LINE_CHANNEL_ACCESS_TOKEN: string,
+    LINE_CHANNEL_SECRET: string,
+    MY_USER_ID: string
+  }
+}>()
+
+
+const scheduled:ExportedHandlerScheduledHandler<Env>  = async(event, env, ctx) => {
+  if(event.cron === "* 12 * * *") {
+    await fetch("https://api.line.me/v2/bot/message/push",{
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${env.LINE_CHANNEL_ACCESS_TOKEN}`
+      },
+      body: JSON.stringify({
+        "to": `${env.MY_USER_ID}`,
+        "message": [
+          {
+            "type": "text",
+            "text": "日記書けよ"
+          }
+        ],
+        "notificationDisabled": false
+      })
+    })
+  }else if(event.cron === "*/30 * * * *"){
+    console.log("Worker Works!")
+  }
+}
+
+
+export default {
+  fetch: app.fetch,
+  scheduled
+}
